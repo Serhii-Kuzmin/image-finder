e861a03e68b8b0763b88e3dd6671ef3dee8e5ec4
Serhii-Kuzmin <seryy8248@gmail.com>
2023-12-07 20:42:11 +0100

    create-api

---
+++ src/components/service/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { paramsForNotify } from 'components/App';
+import { Notify } from 'notiflix';
+
+const KEY = '40717223-2bc60fe276e82255cc8c662af';
+const URL = 'https://pixabay.com/api/';
+
+export async function fetchPhoto(search, page, perPage) {
+  const url = `${URL}?key=${KEY}&q=${search}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal`;
+  const response = await axios.get(url);
+  return response.data;
+}
+
+export function onFetchError() {
+  Notify.failure(
+    'Oops! Something went wrong! Try reloading the page or make another choice!',
+    paramsForNotify
+  );
+}
